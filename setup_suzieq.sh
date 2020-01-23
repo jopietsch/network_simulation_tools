cf41c9a9dfb741c3259d86283bf25326a0e67a48
Justin Pietsch <jpietsch@gmail.com>
2020-01-22 16:50:13 -0800

    try to fix things, but it still doesn't work automatically

--- setup_suzieq.sh
+++ setup_suzieq.sh
@@ -10,16 +10,16 @@ sudo apt install graphviz-dev --assume-yes
 
 
 git clone https://github.com/ddutt/suzieq.git
+git clone https://github.com/ddutt/cyberpandas.git
 pip3 install --user pipenv && \
 export PATH=$PATH:$(pwd)/.local/bin && \
 cd suzieq && \
 rm -f Pipenv.lock && \
 pipenv install && \
 pipenv shell && \
-pip3 install ipython && \
 cd ~/ && \
-git clone https://github.com/ddutt/cyberpandas.git && \
+pip3 install ipython && \
 cd cyberpandas && \
-pip3 install --user .
-cd ..
+pip3 install .
+exit
 # still need to deal with the broken thing in one of the packages.
